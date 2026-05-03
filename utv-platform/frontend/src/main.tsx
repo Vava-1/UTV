56430dd3bcd1e8eb477631857143d883fca885b3
Valentin-lab-max <umuhirevalentin2004@gmail.com>
2026-05-03 05:20:34 +0200

    Fix React import conflict with automatic JSX runtime

--- utv-platform/frontend/src/main.tsx
+++ utv-platform/frontend/src/main.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './i18n'
-import './index.css'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./i18n";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </StrictMode>,
+);
